3d44ec4cd7895e164f341196c8d0e225676d510e
ironman32022 <106044015+ironman32022@users.noreply.github.com>
2023-05-16 08:27:34 +0530

    home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -7,14 +7,76 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(
-        child:Tab(
-          child:Text("Welcome to Resume/CV Builder Application"),
-        ),
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          FutureBuilder(
+              future: FirebaseController().getFullName(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data, style: headerTextStyle);
+                }
+                return AppShimmer(
+                  child: Text("======= ======", style: headerTextStyle),
+                );
+              }),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 20),
+            child: FutureBuilder(
+                future: FirebaseController().getDesignation(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data, style: headerTextStyle);
+                  }
+                  return AppShimmer(
+                    child: Text("==============", style: headerTextStyle),
+                  );
+                }),
+          ),
+          FutureBuilder(
+              future: FirebaseController().getDescription(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data, style: bodyTextStyle);
+                }
+                return AppShimmer(
+                  child: Text("==============", style: headerTextStyle),
+                );
+              }),
+          Container(
+              margin: const EdgeInsets.only(top: 30.0),
+              child: Text(title2, style: headerTextStyle)),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 10.0,
+            ),
+            child: Text(
+              description,
+              style: bodyTextStyle,
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 30.0),
+              child: Text(title3, style: headerTextStyle)),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 20),
+            child: Text(
+              description,
+              style: bodyTextStyle,
+            ),
+          ),
+        ],
       ),
     );
   }
